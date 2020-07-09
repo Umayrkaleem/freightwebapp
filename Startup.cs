0317d167da5d31a958e01b8e0a7110c0d230f028
Umayrkaleem <Umayrkaleem@gmail.com>
2020-07-09 17:33:56 -0500

    adding to app settings and registering database context

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using freightWebApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace freightWebApp
 {
@@ -24,6 +26,9 @@ namespace freightWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<freightWebAppContext>(options =>
+            options.UseSqlite(Configuration.GetConnectionString("FreightContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
