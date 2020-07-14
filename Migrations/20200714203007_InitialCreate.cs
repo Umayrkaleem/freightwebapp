6d3b090a8ba101ea6e8a797f40d66a2cfbd4c51e
Umayrkaleem <Umayrkaleem@gmail.com>
2020-07-14 15:36:17 -0500

    adding intial database schema and scaffolding the freight model

---
+++ Migrations/20200714203007_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace freightWebApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Freight",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    BrokerName = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Commodity = table.Column<string>(nullable: true),
+                    Linehaul = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Freight", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Freight");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -15,20 +15,32 @@ namespace freightWebApp
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            if (Environment.IsDevelopment())
+            {
+                services.AddDbContext<freightWebAppContext>(options =>
+                options.UseSqlite(
+                    Configuration.GetConnectionString("FreightContext")));
+            }
+            else
+            {
+                services.AddDbContext<freightWebAppContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("FreightContext")));
+            }
 
-            services.AddDbContext<freightWebAppContext>(options =>
-            options.UseSqlite(Configuration.GetConnectionString("FreightContext")));
+        services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
